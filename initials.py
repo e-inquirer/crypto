0e4661cb3b05bfc0e01e5866906cf28774d5b6d5
e-inquirer <sorour.a@gmail.com>
2016-12-13 19:46:14 -0500

    created get_initials() function

---
+++ initials.py
@@ -0,0 +1,23 @@
+# Given a person's name, return the person's initials (uppercase)
+def get_initials(fullname):
+    fullname = fullname.split(' ')
+    initials = ''
+
+# accumulator pattern
+    for eachName in fullname:
+        initials = initials + eachName[0].upper()
+    
+    return initials
+
+
+# test stub
+## uncomment to validate code
+
+#from testEqual import testEqual
+### test: "First Last"
+#testEqual.testEqual(get_initials("Ozzie Smith"), "OS")
+### test: "first last"
+#testEqual.testEqual(get_initials("bonnie blair"), "BB")
+### test: "First Middle Last"
+#testEqual.testEqual(get_initials("Daniel Day Lewis"), "DDL")
+
